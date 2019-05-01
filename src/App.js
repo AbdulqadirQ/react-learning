0369cf6c5f4321064b26f22527c7ab75310bcf4c
Abdul Qasim <abdul.qasim@Imp.net>
2019-05-01 11:20:14 +0100

    lec 44 - listeners that change state can be passed to other components in the app, e.g. here clicking paragraph changes state

--- src/App.js
+++ src/App.js
@@ -30,9 +30,16 @@ class App extends Component {
         <h1>Hi, I'm a React app</h1>
         <p>jsx only allows a single root Component to be rendered</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler} >My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
