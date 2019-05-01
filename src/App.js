ab7f941f21bc249106a6d46044391fd9f420dcb5
Abdul Qasim <abdul.qasim@Imp.net>
2019-05-01 11:24:49 +0100

    lec 44 - we can pass variables into listeners when changing state. Here's 2 ways how; using .bind, and using => functions

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     // DON'T DO THIS: this.state.persons[0].name = "Maximilian";
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28},
+        { name: newName, age: 28},
         { name: 'Manu', age: 29},
         { name: 'Stephanie', age: 27}
       ]
@@ -29,14 +29,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>jsx only allows a single root Component to be rendered</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("Maximilian")}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler} >My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, "Max!")} >My Hobbies: Racing</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}/>
