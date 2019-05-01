abaacaca1460865f87f05690d3866afb7199bcb2
Abdul Qasim <abdul.qasim@Imp.net>
2019-05-01 12:07:33 +0100

    lec 47 - adding inline Javascript styling for button (however isn't quite using the full power of css

--- src/App.js
+++ src/App.js
@@ -35,11 +35,21 @@ class App extends Component {
   }
 
   render() {
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>jsx only allows a single root Component to be rendered</p>
-        <button onClick={() => this.switchNameHandler("Maximilian")}>Switch Name</button>
+        <button
+          style={buttonStyle}
+          onClick={() => this.switchNameHandler("Maximilian")}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
