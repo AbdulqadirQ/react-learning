35977833a825cebc4da42d8c238201dab4512283
Abdul Qasim <abdul.qasim@Imp.net>
2019-05-01 10:03:13 +0100

    lec 31 - creating and adding a custom component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import Person from './Person/Person.js'; // can omit .js from end
+
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React app</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+        <p>jsx only allows a single root Component to be rendered</p>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
