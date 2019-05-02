d87002ab838cf29e0c2d9a3f27c5fe085bd00ee3
abdul.qasim <abdul.qasim@baesystems.com>
2019-05-02 16:18:43 +0100

    lec 83 - continue project restructure by adding a Cockpit component to add button logic inside

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -42,42 +43,24 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let buttonClass = '';
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed = {(event) => this.nameChangedHandler(event, person.id)}/>
-          })}
-        </div>
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}/>
       );
-
-      buttonClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
     }
 
     return (
-        <div className={classes.App}>
-          <h1>Hi, I'm a React app</h1>
-          <p className={assignedClasses.join(' ')}>jsx only allows a single root Component to be rendered</p>
-          <button
-            className={buttonClass}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}/>
+        {persons}
+      </div>
     );
   }
 }
