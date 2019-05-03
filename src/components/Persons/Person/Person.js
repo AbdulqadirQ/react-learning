c959f3adf5dab860895ee34bb05241caf77edbf6
abdul.qasim <abdul.qasim@baesystems.com>
2019-05-03 16:49:37 +0100

    lec 108 - we can add references to any element and perform operations when needed. This is the older way of creating a ref. componentDidMount is used for 'focus' since it's executed after render

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,22 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css'
 
 class Person extends Component {
+
+    componentDidMount() {
+        this.inputElement.focus();
+    }
+
     render() {
         console.log('[Person.js...rendering');
         return(
             <Auxiliary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input
+                    ref={(inputEl) => {this.inputElement = inputEl}}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}/>
             </Auxiliary>
         );
     }
